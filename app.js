5523da54795c9c590c11f16d86e3074c6970eb63
Aarush Sharma <aarush.sharma6@gmail.com>
2022-09-25 19:42:13 +0530

    added cases to handle error

--- app.js
+++ app.js
@@ -9,22 +9,33 @@ const outputBox = document.querySelector("#output-box");
 
 
 function compareValues(sumOfDigits, luckyNumber) {
+    if (luckyNumber < 0) {
+        alert("Please enter positive value");
+    }
+
+    if (String(luckyNumber) === "") {
+        alert("Please enter lucky number. This field cannot be empty.");
+    }
+
     if (sumOfDigits % luckyNumber === 0) {
         outputBox.innerText = "Your Birthday is lucky 💖";
     } else {
- 
+
         outputBox.innerText = "Your Birthday  is not lucky 🐱‍🚀";
     }
 }
 
 function checkBirthdayIsLucky() {
     const dob = dateOfBirth.value;
+    if (dob === "") {
+        alert("Please enter your DOB");
+    }
     const sumOfDigits = calculateSum(dob);
     //console.log(sum);
-    
+
 
     compareValues(sumOfDigits, luckyNumber.value);
-     
+
     //outputBox.innerText = "Please enter both the fields. Leaving one field empty will not produce results.😣";
 
 
